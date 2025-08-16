90b70f295526d91c5ac06a85b1e4d261a8f4cfc5
Yuliia Myrakhovska <144026866+Yuliia-Myrakhovska@users.noreply.github.com>
2025-08-17 00:10:21 +0300

    basename

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/moneytrack">
       <Routes>
         <Route
           path="/"
